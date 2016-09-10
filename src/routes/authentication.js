1129be5577b54a1018bc44817667c3f7b2958ac6
Scott Timmins <scotttimmins@hotmail.com>
2016-09-10 14:34:46 +0100

    feat: set an expiry for the auth token

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -8,9 +8,17 @@ var User = require('../models/user');
 
 var config = require('../../config/config.json');
 
+// Convert authentication duration to seconds
+var authenticationDuration = config.authenticationDuration * 60;
+
+var jwtOptions = {
+  expiresIn: authenticationDuration,
+  issuer: 'ServiceWorks'
+};
+
 function login(req, res) {
   var user = User.find();
-  var token = jwt.sign(user, config.secret);
+  var token = jwt.sign(user, config.secret, jwtOptions);
   res.send(token);
 }
 
