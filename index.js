be1ff38917a9c93f5685f003a9ae56d88deee189
Scott Timmins <scotttimmins@hotmail.com>
2016-09-10 15:42:29 +0100

    feat: secure the access token from casual interception

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 /**
  * Module dependencies.
  */
-var express = require('express');
-var passport = require('passport');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
+var express = require('express');
+var fs = require('fs');
+var https = require('https');
+var passport = require('passport');
 
 var config = require('./config/config.json');
 var serviceworksStrategy = require('./src/strategy/serviceworks');
 var authenticationRoutes = require('./src/routes/authentication');
+
+var httpsOptions = {
+  key: fs.readFileSync(config.httpsKeyPath),
+  cert: fs.readFileSync(config.httpsCertPath)
+};
 
 // Passport configuration
 
@@ -20,12 +28,13 @@ var app = express();
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+app.use(cookieParser(config.cookieSecret));
 app.use(passport.initialize());
 app.use(passport.authenticate('local', { session: false }));
 
 // Express routing
 authenticationRoutes(app);
 
-app.listen(3000, function() {
+https.createServer(httpsOptions, app).listen(3000, function() {
   console.log('listening on port 3000');
 });
--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -6,10 +6,17 @@ var passport = require('passport');
 var User = require('../models/user');
 var authentication = require('../libs/authentication');
 
+var cookieOptions = {
+  httpOnly: true,
+  secure: true,
+  signed: true
+};
+
 function login(req, res) {
   var user = User.find();
   var token = authentication.generateAccessToken(user);
-  res.send(token);
+  res.cookie('accessToken', token, cookieOptions);
+  res.send({ success: true });
 }
 
 module.exports = function(app) {
