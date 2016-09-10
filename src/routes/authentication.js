acddf91819645407474bc678c382a07f4a9d4c7d
Scott Timmins <scotttimmins@hotmail.com>
2016-09-10 14:24:56 +0100

    feat: return jwt encoded user on authentication

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,11 +2,16 @@
  * Module dependencies.
  */
 var passport = require('passport');
+var jwt = require('jsonwebtoken');
+
+var User = require('../models/user');
+
+var config = require('../../config/config.json');
 
 function login(req, res) {
-  res.send({
-    authenticated: true
-  })
+  var user = User.find();
+  var token = jwt.sign(user, config.secret);
+  res.send(token);
 }
 
 module.exports = function(app) {
