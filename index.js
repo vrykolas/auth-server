d3dd3b703c719930362410e59307e1661bfffdea
Scott Timmins <scotttimmins@hotmail.com>
2016-09-10 13:58:53 +0100

    feat: extract authentication strategy into its own file

--- index.js
+++ index.js
@@ -6,23 +6,11 @@ var passport = require('passport');
 var bodyParser = require('body-parser');
 
 var config = require('./config/config.json');
-var LocalStrategy = require('passport-local').Strategy;
+var serviceworksStrategy = require('./src/strategy/serviceworks');
 
 // Passport configuration
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    if(username !== config.username) {
-      return done(null, false, { message: 'Incorrect username.' });
-    }
-
-    if(password != config.password) {
-      return done(null, false, { message: 'Incorrect password.' });
-    }
-
-    return done(null, true);
-  }
-));
+passport.use(serviceworksStrategy);
 
 // Express configuration
 
---
+++ src/strategy/serviceworks.js
@@ -0,0 +1,24 @@
+/**
+ * Module dependencies.
+ */
+var LocalStrategy = require('passport-local').Strategy;
+
+var config = require('../../config/config.json');
+
+// Passport configuration
+
+var serviceworksStrategy = new LocalStrategy(
+  function(username, password, done) {
+    if(username !== config.username) {
+      return done(null, false, { message: 'Incorrect username.' });
+    }
+
+    if(password != config.password) {
+      return done(null, false, { message: 'Incorrect password.' });
+    }
+
+    return done(null, true);
+  }
+);
+
+module.exports = serviceworksStrategy;
